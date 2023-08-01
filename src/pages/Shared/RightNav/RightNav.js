fe7e794e5498bbb641bb1508547847adfeb937eb
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-08-02 00:26:03 +0600

    Right Section Added

--- src/pages/Shared/RightNav/RightNav.js
+++ src/pages/Shared/RightNav/RightNav.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGoogle,FaGithub,FaFacebook,FaInstagram,FaTwitter } from 'react-icons/fa';
+import QZone from '../Qzone/QZone';
+import bg from '../../../assets/bg.png';
 
 const RightNav = () => {
     return (
         <div>
-            <h3>Right Nav</h3>
+           <h4>Login With</h4>
+            <Button className='mb-2 ' variant="outline-primary"> <FaGoogle/> Login With Google</Button>
+            <Button variant="outline-secondary"> <FaGithub /> Login With Githb</Button>
+
+            <div>
+                <h4>Find us on</h4>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebook/> Facebook</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram/> Instagram</ListGroup.Item>
+                    <ListGroup.Item> <FaTwitter/> Twitter</ListGroup.Item>
+                 
+             </ListGroup>
+            </div>
+            <QZone></QZone>
+            <div>
+                <img src={bg} alt="" />
+            </div>
         </div>
     );
 };
