8843cb172b8e8de3d1b8f9dced581acd732dcd1d
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-08-02 18:19:07 +0600

    Main News Details Added

--- src/pages/News/News/News.js
+++ src/pages/News/News/News.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Button, Card} from 'react-bootstrap';
+import {useLoaderData, Link } from 'react-router-dom';
+import { FaArrowLeft } from 'react-icons/fa';
+import EditorsInsight from './EditorsInsight/EditorsInsight';
 
 const News = () => {
+
+    const news = useLoaderData();
+    const {_id, title, details,image_url, category_id } = news;
     return (
         <div>
-            <h2>News Details Coming soon .....</h2>
+            <Card>
+            <Card.Img variant="top" src={image_url} />
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>
+                   {details}
+                </Card.Text>
+                <Link to={`/category/${category_id}`}>
+                <Button variant="danger"> <FaArrowLeft></FaArrowLeft>All news in this category</Button>
+                </Link>
+                
+            </Card.Body>
+        </Card>
+        <EditorsInsight></EditorsInsight>
         </div>
     );
 };
