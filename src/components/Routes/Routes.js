2116830fb6b6cbfd479befca5abdcf0decb1ec94
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-08-02 15:27:45 +0600

    News Details Seaction Added

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -18,18 +18,19 @@ const  router =  createBrowserRouter([
             },
             {
                 path:'/category/:id',
-                element:<Category></Category>
+                element:<Category></Category>,
+                loader: ({params}) => fetch(`http://localhost:5000/categories/${params.id}`)
             },
            
         ]
     },
     {
-        path:'news',
+        path:'/news',
         element:<NewsLayout></NewsLayout>,
         children: [
             {
-                path:'id',
-                element:<News></News>
+                path:'/news/:id',
+                element:<News></News>,
             }
         ]
     }
